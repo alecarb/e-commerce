a7ade0bd874ae72716a1a400e6caba9830518a95
AleCarb <alecarb36@gmail.com>
2023-08-17 13:32:50 -0300

    clase1 e-commerce

---
+++ client/js/products.js
@@ -0,0 +1,50 @@
+//este archivo va a listar los productos desde un array
+const productos = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 480,
+        quanty: 1,
+        img: "/client/media/banana.png",
+    },
+    {
+        id: 2,
+        productName: "Leche",
+        price: 950,
+        quanty: 1,
+        img: "/client/media/leche.png",
+    },
+    {
+        id: 3,
+        productName: "Pollo",
+        price: 750,
+        quanty: 1,
+        img:"/client/media/pollo.png",
+    },   
+    {
+        id: 4,
+        productName: "Mayonesa",
+        price: 880,
+        quanty: 1,
+        img: "/client/media/mayonesa.png", 
+    },
+    {
+        id: 5,
+        productName: "Enlatados",
+        price: 120,
+        quanty: 1,
+        img: "/client/media/enlatados.png",
+    },
+    {
+        id: 6,
+        productName: "Varios",
+        price: 280,
+        quanty: 1,
+        img: "/client/media/varios.jpeg",
+    }
+
+
+
+
+
+];
